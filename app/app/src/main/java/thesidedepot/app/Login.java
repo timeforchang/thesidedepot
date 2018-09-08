e487178361e2ee16b6fa1605ff249558a88a02e7
timeforchang <chang.andrew.23@gmail.com>
2018-09-07 22:35:29 -0400

    starting the app

---
+++ app/app/src/main/java/thesidedepot/app/Login.java
@@ -0,0 +1,18 @@
+package thesidedepot.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EditText email, pass;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
