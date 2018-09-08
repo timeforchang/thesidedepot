b92f7b43665d1b0717b2f52a3fdb41022c923411
Dennis Eddington <denniseddington@lawn-128-61-6-235.lawn.gatech.edu>
2018-09-08 00:39:00 -0400

    Ya YEET

---
+++ BackEnd/scraping/querySample.py
@@ -0,0 +1,32 @@
+
+import pymongo
+import ssl
+from pymongo import MongoClient
+
+
+
+client = MongoClient('mongodb+srv://admin:siderift@cluster0-1jnpy.mongodb.net/test?retryWrites=true', ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+
+# Get the sampleDB database
+db = client.database
+collection = db.users
+username = "admin"
+password = "pass"
+
+query = {'username' : 'admin'}
+
+user = {
+	'username' : username,
+	'password' : password
+}
+
+#collection.insert_one(user)
+
+for item in collection.find(query):
+	print(item)
+	if (item['password'] == 'pass'):
+		print(0)
+	else:
+		print(1)
+	
+
