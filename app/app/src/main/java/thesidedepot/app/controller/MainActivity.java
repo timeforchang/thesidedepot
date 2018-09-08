1457cd273d73125338ec2dc9f96620528eaecaa2
timeforchang <chang.andrew.23@gmail.com>
2018-09-08 01:51:58 -0400

    calendar tweaks

--- app/app/src/main/java/thesidedepot/app/controller/MainActivity.java
+++ app/app/src/main/java/thesidedepot/app/controller/MainActivity.java
@@ -1,35 +1,70 @@
 package thesidedepot.app.controller;
 //Calendar
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import thesidedepot.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    CalendarView simpleCalendarView;
+    private static final String TAG = "";
+    CompactCalendarView compactCalendarView;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
+    private TextView month;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        simpleCalendarView = (CalendarView) findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
-        simpleCalendarView.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
-        simpleCalendarView.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
-        simpleCalendarView.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
-        simpleCalendarView.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
-        // perform setOnDateChangeListener event on CalendarView
-        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+        month = (TextView) findViewById(R.id.month);
+        month.setText(dateFormatMonth.format(Calendar.getInstance().getTime()));
+
+        compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view); // get the reference of CalendarView
+        compactCalendarView.setFirstDayOfWeek(Calendar.SUNDAY);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+
+        Event ev1 = new Event(Color.RED, 1477040400000L, "Teachers' Professional Day");
+        compactCalendarView.addEvent(ev1);
+
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                // display the selected date by using a toast
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+                if (dateClicked.toString().compareTo("Fri Oct 21 00:00:00 EDT 2016") == 0) {
+                    Toast.makeText(context, "Teachers' Professional Day", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+                Log.d(TAG, "Day was clicked: " + dateClicked + " with events " + events);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                month.setText(dateFormatMonth.format(firstDayOfNewMonth));
+                Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
             }
         });
     }
