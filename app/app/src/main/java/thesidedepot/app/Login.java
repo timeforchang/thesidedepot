96954f7cd2b11d5780f861560f3666567d7a06fe
timeforchang <chang.andrew.23@gmail.com>
2018-09-07 23:05:43 -0400

    merge conflicts

--- app/app/src/main/java/thesidedepot/app/Login.java
+++ app/app/src/main/java/thesidedepot/app/Login.java
@@ -2,15 +2,33 @@ package thesidedepot.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.EditText;
+import android.support.design.widget.Snackbar;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EditText email, pass;
+        CardView login;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        login = findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snack = Snackbar.make(findViewById(), "logging you in...", Snackbar.LENGTH_LONG);
+                System.out.println("logged in");
+                snack.show();
+                loginUser(loginEmail.getText().toString(), loginPass.getText().toString());
+            }
+        });
     }
 }
