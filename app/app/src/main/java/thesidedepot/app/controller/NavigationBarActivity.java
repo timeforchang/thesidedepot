b10ab325195e9222b0b08f9b3800cc3985d6bc28
glin39 <georgiannalin@gmail.com>
2018-09-08 10:51:40 -0400

    Blue was dumb, but she fixed it

--- app/app/src/main/java/thesidedepot/app/controller/NavigationBarActivity.java
+++ app/app/src/main/java/thesidedepot/app/controller/NavigationBarActivity.java
@@ -88,8 +88,6 @@ public class NavigationBarActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
