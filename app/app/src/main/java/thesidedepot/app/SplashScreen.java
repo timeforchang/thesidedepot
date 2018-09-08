f55c1b8300fa740d6337ef7d797a8366ddaf4d13
timeforchang <chang.andrew.23@gmail.com>
2018-09-07 22:58:02 -0400

    splash screen done

--- app/app/src/main/java/thesidedepot/app/SplashScreen.java
+++ app/app/src/main/java/thesidedepot/app/SplashScreen.java
@@ -1,5 +1,6 @@
 package thesidedepot.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,19 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-
+        Thread wait = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        wait.start();
     }
 }
