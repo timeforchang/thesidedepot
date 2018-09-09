ac02ca110ba78dc1525f23e243003c139dd4da4d
glin39 <georgiannalin@gmail.com>
2018-09-09 04:08:57 -0400

    ahhhhhhhhhhhhh

--- app/app/src/main/java/thesidedepot/app/controller/HowToActivity.java
+++ app/app/src/main/java/thesidedepot/app/controller/HowToActivity.java
@@ -1,8 +1,12 @@
 package thesidedepot.app.controller;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -34,5 +38,32 @@ public class HowToActivity extends Activity {
 
         GridAdapter gridAdapter = new GridAdapter(this, materialList);
         materials.setAdapter(gridAdapter);
+
+        Button completionButton = (Button) findViewById(R.id.completionButton);
+
+        completionButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+
+        builder.setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
     }
 }
