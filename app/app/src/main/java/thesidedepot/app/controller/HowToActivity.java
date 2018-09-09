e54ccd98fb3cbdf7d20174222b9200b860e43e79
timeforchang <chang.andrew.23@gmail.com>
2018-09-09 05:10:18 -0400

    help me

--- app/app/src/main/java/thesidedepot/app/controller/HowToActivity.java
+++ app/app/src/main/java/thesidedepot/app/controller/HowToActivity.java
@@ -5,15 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.Button;
-=======
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
->>>>>>> 5a9b29de60ad8178b2935ab51a06f278f1c43e0c
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -62,45 +59,33 @@ public class HowToActivity extends AppCompatActivity {
         diff = (TextView) findViewById(R.id.diff);
         diff.setText(cur.getDifficulty());
 
-<<<<<<< HEAD
-        String[] materialList = new String[cur.getToolsAndMaterials().size()];
-
-        materialList = (String[]) cur.getToolsAndMaterials().toArray(materialList);
-
-        materials = (GridView) findViewById(R.id.materials);
-
-        GridAdapter gridAdapter = new GridAdapter(this, materialList);
-        materials.setAdapter(gridAdapter);
-
         Button completionButton = (Button) findViewById(R.id.completionButton);
 
         completionButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder builder;
+                builder = new AlertDialog.Builder(HowToActivity.this, android.R.style.Theme_Material_Dialog_Alert);
 
+                builder.setTitle("Delete entry")
+                        .setMessage("Are you sure you want to delete this entry?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
-        AlertDialog.Builder builder;
-        builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
 
-        builder.setTitle("Delete entry")
-                .setMessage("Are you sure you want to delete this entry?")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // continue with delete
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // do nothing
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
 
-=======
         link = (TextView) findViewById(R.id.link);
         link.setText(cur.getWebCollection().get(cur.getWebCollection().size() - 1));
->>>>>>> 5a9b29de60ad8178b2935ab51a06f278f1c43e0c
     }
 }
